fe811a4d4c81193b14e3ef2f920f837b25fb26d7
julian-perez <julian.perez@canary.is>
2018-03-13 13:19:18 -0400

    Initial Commit

---
+++ iOSTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTestApp
+//
+//  Created by Julian on 3/13/18.
+//  Copyright © 2018 Julian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
